237cf4afc0066b051ca0d3e625e69976c788e153
Meggie Enxuto <you@example.com>
2022-12-11 11:49:35 -0700

    day 9 part 2 solution

--- 9/advent.py
+++ 9/advent.py
@@ -1,12 +1,25 @@
 from copy import copy
 class Grid:
-	def __init__(self):
-		self.head = Head()
-		self.tail = Tail(self.head)
-		self.head.set_tail(self.tail)
-		self.head.grid = self
-		self.tail.grid = self
+	def __init__(self, length=10):
+		
+		self.head = Node(0)
+		self.nodes = [self.head]
+		previous = self.head
+		for i in range(length-1):
+			node = Node(parent=previous, data=i+1)
+			previous.child = node
+			previous = node
+			self.nodes.append(node)
+		self.tail = self.nodes[-1]
 
+	def process(self, line):
+		print('LINE', line)
+		direction, num = line.strip().split()
+		num = int(num)
+
+		for i in range(num):
+			self.head.move_once(direction)
+		
 	def __repr__(self):
 		max_x = max([abs(self.head.x), abs(self.tail.x)])
 		max_y = max([abs(self.tail.y), abs(self.tail.y)])
@@ -24,78 +37,64 @@ class Grid:
 		return out
 
 
-class Knot:
-	def __init__(self, x=0, y=0):
-		self.x = 0
-		self.y = 0
-
-	def __repr__(self):
-		return f"({self.x, self.y})"
-
-
-class Tail(Knot):
-	def __init__(self, head):
-		super().__init__()
-		self.head = head
+class Node:
+	def __init__(self, data, parent=None, child=None, x=0, y=0):
+		self.x = x
+		self.y = y
+		self.parent = parent
+		self.child = child
+		self.data = data
 		self.visited = set()
 		self.visited.add((0,0))
 
+
+	def __repr__(self):
+		return f"({self.x, self.y})"
+		
 	def handle_move(self):
-		print('need to move tail now')
-		if self.y == self.head.y:
-			if self.head.x > self.x:
+		print('moving', self.data)
+		if self.y == self.parent.y:
+			if self.parent.x > self.x:
 				self.x += 1
 			else:
 				self.x -= 1
-		elif self.x == self.head.x:
-			if self.head.y > self.y:
+		elif self.x == self.parent.x:
+			if self.parent.y > self.y:
 				self.y += 1
 			else:
 				self.y -= 1
 		else:
-			if self.head.x > self.x:
+			if self.parent.x > self.x:
 				self.x += 1
 			else:
 				self.x -= 1
 
-			if self.head.y > self.y:
+			if self.parent.y > self.y:
 				self.y += 1
 			else:
 				self.y -= 1
 
 		self.visited.add((self.x, self.y))
 
-	def react(self):
-		print(f'reacting to new head position {self.head.x}, {self.head.y}')
+		if self.child:
+			self.child.react()
 
+
+	def react(self):
+		print("child", self.data, "reaction")
 		# if same, do nothing
-		if self.x == self.head.x and self.y == self.head.y:
+		if self.x == self.parent.x and self.y == self.parent.y:
 			return
 		# if in same col and only one row away do nothing
-		elif self.x == self.head.x and (abs(self.y - self.head.y) == 1):
+		elif self.x == self.parent.x and (abs(self.y - self.parent.y) == 1):
 			return
-
-
 		# if in same row and only one away do nothing
-		elif self.y == self.head.y and (abs(self.x - self.head.x) == 1):
+		elif self.y == self.parent.y and (abs(self.x - self.parent.x) == 1):
 			return
-
-
-		elif abs(self.x - self.head.x) == 1 and abs(self.y - self.head.y) == 1:
+		elif abs(self.x - self.parent.x) == 1 and abs(self.y - self.parent.y) == 1:
 			return
-
 		else:
 			self.handle_move()
-
-
-
-class Head(Knot):
-	def __init(self, tail=None):
-		super().__init__()
-		self.tail = tail
-
-	def set_tail(self, tail):
-		self.tail = tail
 
 	def move_once(self, direction):
 		if direction == "R":
@@ -106,21 +105,8 @@ class Head(Knot):
 			self.y += 1
 		if direction == "D":
 			self.y -= 1
-
-		self.tail.react()
-		# print(self.grid)
-
-	def process(self, line):
-		print('LINE', line)
-		direction, num = line.strip().split()
-		num = int(num)
-
-		for i in range(num):
-			self.move_once(direction)
-			print(f"head {self} tail {self.tail}")
-
-
-		# import pdb; pdb.set_trace()
+		if self.child:
+			self.child.react()
 
 
 if __name__ == "__main__":
@@ -128,7 +114,7 @@ if __name__ == "__main__":
 
 	f = open("input.txt")
 	for line in f:
-		g.head.process(line)
+		g.process(line)
 
 	print(len(g.tail.visited))
 
